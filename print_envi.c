75e91d228603945c7c99fdfbe8f00589f94c8bc6
Cristian Pinzón <faykris28@gmail.com>
2021-04-14 12:15:05 -0500

    change logical on print_envi

--- print_envi.c
+++ print_envi.c
@@ -7,14 +7,13 @@
  */
 void print_envi(void)
 {
+	int i = 0;
 	char **env = environ;
-	char *thisEnv = *env;
 
-	while (*env != 0)
+	while (env[i])
 	{
-		thisEnv = *env;
-		write(STDOUT_FILENO, thisEnv, _strlen(thisEnv));
+		write(STDOUT_FILENO, env[i], _strlen(env[i]));
 		write(STDOUT_FILENO, "\n", 1);
-		env++;
+		i++;
 	}
 }

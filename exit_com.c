55c747c9923077db77f68b6446a41c71ccb5f241
Cristian Pinzón <faykris28@gmail.com>
2021-04-14 17:02:24 -0500

    add messages errors with write and add exit code 127

--- exit_com.c
+++ exit_com.c
@@ -11,7 +11,8 @@
  */
 int exit_com(char **av, char *exe, char *f_di, char **o_di, size_t count)
 {
-	int e = 0;
+	int e = 0, dig = 0;
+	char *ctr = NULL;
 
 	if (av[1] == NULL)
 		return (1);
@@ -23,7 +24,17 @@ int exit_com(char **av, char *exe, char *f_di, char **o_di, size_t count)
 	}
 	else
 	{
-		count = count;
+		dig = digits_count(count);
+		ctr = malloc(sizeof(char) * dig + 1);
+		write(STDERR_FILENO, exe, _strlen(exe));
+		write(STDERR_FILENO, ": ", 2);
+		write(STDERR_FILENO, _itoa(count, ctr, 10), dig);
+		write(STDERR_FILENO, ": ", 2);
+		write(STDERR_FILENO, av[0], _strlen(av[0]));
+		write(STDERR_FILENO, ": Illegal number: ", 18);
+		write(STDERR_FILENO, av[1], _strlen(av[1]));
+		write(STDERR_FILENO, "\n", 1);
+		free(ctr); 
 		if (!isatty(STDIN_FILENO))
 		{
 			free_helper(av[0], exe, f_di, o_di);
--- shell.c
+++ shell.c
@@ -12,7 +12,7 @@ int main(int argc, char **argv)
 	char *string = NULL, **p_dire = NULL, *exec = NULL, *com = NULL;
 	struct stat st;
 	size_t count = 0;
-	int ind = 0;
+	int ind = 0, code = 0;
 
 	signal(SIGINT, &catch);
 	exec = _strdup(argv[0]);
@@ -33,18 +33,20 @@ int main(int argc, char **argv)
 		{
 			free(string);
 			continue;
-		}
-		else if (argv[0] != NULL)
+		}	else if (argv[0] != NULL)
 		{
 			com = _strdup(argv[0]);
 			argv[0] = search_dir_com(argv[0], p_dire, exec, count);
 			if (stat(argv[0], &st) == 0)
 				exec_com_args(argv, p_dire, com, exec, count);
+			else
+				code = _atoi(argv[0]);
 			free(com);
 			free(string);
 		}
-	}
-	free_helper(string, exec, p_dire[0], p_dire);
+	}	free_helper(string, exec, p_dire[0], p_dire);
 	argc = argc;
+	if (code != 0)
+		exit(code);
 	return (EXIT_SUCCESS);
 }
